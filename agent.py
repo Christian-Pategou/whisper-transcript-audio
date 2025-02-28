71aab0af0e19b2535ce5c1d38d2c13f26f681a2c
Christian-Pategou <150147218+Christian-Pategou@users.noreply.github.com>
2025-02-28 19:55:33 +0100

    Add files via upload

---
+++ agent.py
@@ -0,0 +1,103 @@
+from smolagents import CodeAgent, tool
+import os
+from dotenv import load_dotenv
+
+from langchain_chroma import Chroma
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from operator import itemgetter
+
+load_dotenv()
+
+
+############################## agent ################################
+
+@tool
+def get_patient_id(name:str)-> str:
+    """ ton rôle est de recuperer l'id d'un patient dans le fichier patients.csv à partir de son nom ou son prenom.
+    Args:
+        name: le nom ou le prénom du patient.
+    return:
+        id du patient
+    
+    """
+    import pandas as pd
+    patients = pd.read_csv('patients.csv')
+    # consultations = pd.read_csv('consultations.csv')
+
+    # Find the patient ID 
+    patient_i1 = patients[patients['firstname'].str.lower()==name.lower()]['_id']
+    patient_i2 = patients[patients['lastname'].str.lower()==name.lower()]['_id']
+    if len(patient_i1) > 0:
+        patient_id = patient_i1.values[0]
+    else:
+        try:
+            patient_id=patient_i2.values[0]
+        except IndexError as e:
+            patient_id=f"Aucun patient avec le nom {name}"
+    
+    return patient_id
+
+
+def ana_agent(model):
+
+    agent = CodeAgent(
+        # system_prompt=prompt,
+        tools=[get_patient_id],
+        model=model,
+        max_steps=5,
+        additional_authorized_imports=["pandas", "os", "numpy", "datetime"],
+        verbosity_level=2 ,
+    )
+
+    return agent 
+
+############################## langchain response ################################
+
+
+embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-l6-v2")
+vector_store = Chroma(
+    collection_name="imesy_chromadb",
+    embedding_function=embedding,
+    persist_directory="imest_chroma",  
+)
+
+retriever = vector_store.as_retriever(
+    search_type="mmr",
+    search_kwargs={ 'k':5, 'lambda_mult': 0.5, 'fetch_k':10},
+)
+
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-1.5-pro",
+    api_key=os.getenv("GOOGLE_API_KEY"),
+    max_retries=2
+    
+)
+model_chain = ChatGoogleGenerativeAI(
+    model= "gemini-2.0-flash-exp", #"gemini-2.5-flash",
+    api_key=os.getenv("GOOGLE_API_KEY"),
+    max_retries=2
+    
+)
+
+prompt_support = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "Tu es un assistant capable de repondre de facon claire et structurer a la question de l'utilisateur en te servant uniquement des informations mise à ta disposition. \
+            context: {context}",
+        ),
+        ("human", "{question}"),
+    ]
+)
+
+retrieval_chain = (
+    {"context": itemgetter("question") | retriever,
+    "question": itemgetter("question")}
+    | prompt_support
+    | model_chain
+    | StrOutputParser()
+)
---
+++ app.py
@@ -0,0 +1,84 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from smolagents import HfApiModel, LiteLLMModel
+from agent import ana_agent as imesy_agent, retrieval_chain
+from prompts import prompt
+
+from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model_hf = HfApiModel(
+    model_id="Qwen/Qwen2.5-Coder-32B-Instruct",
+    token=os.getenv('HF_TOKEN')
+)
+
+model_gemini = LiteLLMModel(
+    model_id="gemini-2.0-flash-lite-preview-02-05",   #"gemini/gemini-2.0-flash-exp", #"openrouter/google/gemini-2.0-pro-exp-02-05:free"
+    api_key = os.getenv('GOOGLE_API_KEY')
+)
+
+model_chain = ChatGoogleGenerativeAI(
+    model= "gemini-2.0-flash-exp", #"gemini-2.5-flash",
+    api_key=os.getenv("GOOGLE_API_KEY"),
+    max_retries=2
+    
+)
+
+app = FastAPI()
+
+# Ajoute le middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permet uniquement les origines spÃ©cifiÃ©es
+    allow_credentials=True,
+    allow_methods=["*"],  # Permet toutes les mÃ©thodes HTTP (GET, POST, etc.)
+    allow_headers=["*"],   # Permet tous les en-tÃªtes
+)
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", """
+Tu es un assistant intelligent qui doit router une question à l'un des deux services :
+- 'support' pour les questions sur l'utilisation de l'application. L'application s'appelle IMESY. c'est une aapp web permettant aux medecins de simplifier le processus de consultation.
+- 'agent_medical' pour les questions concernant dont la reponse necessite de faire une recherche dans la base de données.
+
+Question: {question}
+Catégorie (support ou agent_medical) ?
+Réponds uniquement par "support" ou "agent_medical".
+"""),
+("human", "{question}")
+    ]
+)
+
+
+router = prompt_template | model_chain
+
+@app.post("/imesy-router/")
+async def imesy_router(question:str):
+    res = await router.ainvoke(question)
+    if res.content == "agent_medical":
+        print("enter to agent bot")
+        agent = imesy_agent(model_gemini)
+        try:
+            response = agent.run(
+                prompt + "\nq" + question,
+                additional_args=dict(source_file=["patients.csv", "consultations.csv"])
+            )
+            
+        except Exception as e:
+            print(f"une erreur  s'est produite:\n\n {e}")
+
+    elif res.content == "support":
+        print("enter to support bot")
+        response = await retrieval_chain.ainvoke({"question":question})
+        
+    else:
+        return f"une erreur s'est produite \n {res}"
+    return response
+
+
+
