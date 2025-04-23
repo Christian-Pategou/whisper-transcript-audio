9e33009cfa3afdd1b9ccaeb73cfe3e8dcad38342
kepya <christiankepya@gmail.com>
2025-04-23 12:37:00 +0100

    update view

--- funct.py
+++ funct.py
@@ -21,7 +21,7 @@ import os
 load_dotenv("./../.env")
 
 SMTP_SERVER = os.getenv("SMTP_SERVER") 
-SMTP_PORT = os.getenv("SMTP_PORT") 
+SMTP_PORT = 587
 SENDER_EMAIL = os.getenv("SENDER_EMAIL") 
 SENDER_PASSWORD = os.getenv("SENDER_PASSWORD") 
 RECEIVER_EMAIL = os.getenv("RECEIVER_EMAIL") 
